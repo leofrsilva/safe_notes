b36bafcbfa6575674d69f39ffd1bc2d912101f47
Leonardo Freire Ribeiro da Silva <leonardo13022019@gmail.com>
2022-03-10 10:03:19 -0300

    Config AppWidget

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_triple/flutter_triple.dart';
+import 'package:safe_notes/app/design/common/common.dart';
+import 'package:safe_notes/app/modules/setting/presenter/setting_store.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routeInformationParser: Modular.routeInformationParser,
-      routerDelegate: Modular.routerDelegate,
-      debugShowCheckedModeBanner: false,
-      title: 'Safe Notes',
-      theme: ThemeData(primarySwatch: Colors.blue),
-    );
+    return ScopedBuilder<SettingStore, Exception, bool>.transition(
+        store: SettingStore(), // Instantiating Store
+        onLoading: (context) => const Center(
+              child: CircularProgressIndicator.adaptive(),
+            ),
+        onState: (context, dark) {
+          return MaterialApp.router(
+            routeInformationParser: Modular.routeInformationParser,
+            routerDelegate: Modular.routerDelegate,
+            debugShowCheckedModeBanner: false,
+            title: 'Safe Notes',
+            theme: dark ? Themes.darkTheme : Themes.lightTheme,
+          );
+        });
   }
 }
