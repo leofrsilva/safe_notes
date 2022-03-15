406fc3ac8a37fe82ae94a73c76c2ea51f10c3f67
Leonardo Freire Ribeiro da Silva <leonardo13022019@gmail.com>
2022-03-15 19:02:27 -0300

    UsuarioEntity Temp

--- lib/app/shared/database/entities/usuario_entity.dart
+++ lib/app/shared/database/entities/usuario_entity.dart
@@ -1,5 +1,72 @@
-import 'base_entity.dart';
+import 'package:floor/floor.dart';
 
+import 'base_entity.dart';
+import '../../../design/common/extension/extension.dart';
+
+@Entity(
+  tableName: 'Usuario',
+  indices: [
+    Index(value: ['email'], unique: true)
+  ],
+)
 class UsuarioEntity extends BaseEntity {
-  UsuarioEntity(int id) : super(id);
+  @ColumnInfo(name: 'email')
+  final String email;
+
+  final String name;
+
+  @ColumnInfo(name: 'genre')
+  final int _genre;
+
+  @ColumnInfo(name: 'data_birth')
+  final String _dataBirth;
+
+  @ColumnInfo(name: 'logged')
+  final int _logged;
+
+  UsuarioEntity({
+    int? id,
+    DateTime? dateCreate,
+    DateTime? dateModification,
+    required this.email,
+    required this.name,
+    required String genre,
+    required DateTime dataBirth,
+    required bool logged,
+  })  : _genre = genre.toInt,
+        _dataBirth = dataBirth.toString(),
+        _logged = logged.toInt,
+        super(
+          id: id!,
+          dateCreate: dateCreate,
+          dateModification: dateModification,
+        );
+
+  String get genre => _genre.toChar;
+
+  DateTime get dataBirth => _dataBirth.toDateTime;
+
+  bool get logged => _logged.toBool ?? false;
+
+  UsuarioEntity copyWith({
+    int? id,
+    DateTime? dateCreate,
+    DateTime? dateModification,
+    String? email,
+    String? name,
+    String? genre,
+    DateTime? dataBirth,
+    bool? logged,
+  }) {
+    return UsuarioEntity(
+      id: id ?? this.id,
+      dateCreate: dateCreate ?? this.dateCreate.toDateTime,
+      dateModification: dateModification ?? this.dateModification.toDateTime,
+      email: email ?? this.email,
+      name: name ?? this.name,
+      genre: genre ?? this.genre,
+      dataBirth: dataBirth ?? this.dataBirth,
+      logged: logged ?? this.logged,
+    );
+  }
 }
