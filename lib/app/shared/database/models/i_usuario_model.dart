9c1d17ae90c0c1f3274c6c7b699c08b9d057a583
Leonardo Freire Ribeiro da Silva <leonardo13022019@gmail.com>
2022-03-16 10:48:38 -0300

    Create UsuarioModel

--- lib/app/shared/database/models/i_usuario_model.dart
+++ lib/app/shared/database/models/i_usuario_model.dart
@@ -1,3 +1,5 @@
+import '../entities/usuario_entity.dart';
+
 abstract class IUsuarioModel {
   int get userId;
   String get name;
@@ -7,49 +9,6 @@ abstract class IUsuarioModel {
   DateTime get dateCreate;
   DateTime get dateModification;
   bool get logged;
-} 
 
-  // UsuarioEntity({
-  //   int? userId,
-  //   String? dateCreate,
-  //   String? dateModification,
-  //   required this.dateBirth,
-  //   required this.email,
-  //   required this.name,
-  //   required String genre,
-  //   required this.logged,
-  // })  : codeUnitGenre = genre.toInt,
-  //       // loggedInInt = logged,
-  //       super(
-  //         id: userId,
-  //         dateCreate: dateCreate ?? DateTime.now().toString(),
-  //         dateModification: dateModification ?? DateTime.now().toString(),
-  //       );
-
-  // String get genre => codeUnitGenre.toChar;
-
-  // DateTime get dateBirthToDate => dateBirth.toDateTime;
-
-  // bool get isLogged => logged.toBool ?? false;
-  
-  // UsuarioEntity copyWith({
-  //   int? userId,
-  //   DateTime? dateCreate,
-  //   DateTime? dateModification,
-  //   String? email,
-  //   String? name,
-  //   String? genre,
-  //   DateTime? dateBirth,
-  //   bool? logged,
-  // }) {
-  //   return UsuarioEntity(
-  //     userId: userId ?? id,
-  //     dateCreate: dateCreate?.toString() ?? this.dateCreate,
-  //     dateModification: dateModification?.toString() ?? this.dateModification,
-  //     email: email ?? this.email,
-  //     name: name ?? this.name,
-  //     genre: genre ?? this.genre,
-  //     dateBirth: dateBirth?.toString() ?? this.dateBirth,
-  //     logged: logged?.toInt ?? this.logged,
-  //   );
-  // }
+  UsuarioEntity get entity;
+}
---
+++ lib/app/shared/database/models/usuario_model.dart
@@ -0,0 +1,78 @@
+import '../entities/usuario_entity.dart';
+import '../../../design/common/extension/extension.dart';
+
+import 'i_usuario_model.dart';
+
+class UsuarioModel implements IUsuarioModel {
+  final UsuarioEntity _entity;
+  UsuarioModel(this._entity);
+
+  @override
+  UsuarioEntity get entity => _entity;
+
+  @override
+  bool get logged => _entity.logged.toBool ?? false;
+
+  @override
+  DateTime get dateCreate => _entity.dateCreate.toDateTime;
+
+  @override
+  DateTime get dateModification => _entity.dateModification.toDateTime;
+
+  @override
+  DateTime get dateBirth => _entity.dateBirth.toDateTime;
+
+  @override
+  String get genre => _entity.genre.toChar;
+
+  @override
+  String get email => _entity.email;
+
+  @override
+  String get name => _entity.name;
+
+  @override
+  int get userId => _entity.id ?? 0;
+
+  UsuarioModel.fromEntity({
+    int? userId,
+    DateTime? dateCreate,
+    DateTime? dateModification,
+    required DateTime dateBirth,
+    required String email,
+    required String name,
+    required String genre,
+    required bool logged,
+  }) : _entity = UsuarioEntity(
+          userId: userId,
+          email: email,
+          name: name,
+          genre: genre.toInt,
+          logged: logged.toInt,
+          dateBirth: dateBirth.toString(),
+          dateCreate: (dateCreate ?? DateTime.now()).toString(),
+          dateModification: (dateModification ?? DateTime.now()).toString(),
+        );
+
+  UsuarioModel copyWith({
+    int? userId,
+    DateTime? dateBirth,
+    DateTime? dateCreate,
+    DateTime? dateModification,
+    String? email,
+    String? name,
+    String? genre,
+    bool? logged,
+  }) {
+    return UsuarioModel.fromEntity(
+      userId: userId ?? this.userId,
+      dateBirth: dateBirth ?? this.dateBirth,
+      dateCreate: dateCreate ?? this.dateCreate,
+      dateModification: dateModification ?? this.dateModification,
+      email: email ?? this.email,
+      name: name ?? this.name,
+      genre: genre ?? this.genre,
+      logged: logged ?? this.logged,
+    );
+  }
+}
