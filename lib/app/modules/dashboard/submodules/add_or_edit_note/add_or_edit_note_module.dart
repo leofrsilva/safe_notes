c6b57aa06a8e2cb83bb0c2bda77653b3d4a307df
Leonardo Freire Ribeiro da Silva <leonardo13022019@gmail.com>
2022-05-28 09:12:24 -0300

    Insert Stores dependences

--- lib/app/modules/dashboard/submodules/add_or_edit_note/add_or_edit_note_module.dart
+++ lib/app/modules/dashboard/submodules/add_or_edit_note/add_or_edit_note_module.dart
@@ -10,6 +10,8 @@ import 'infra/datasources/i_note_datasource.dart';
 import 'infra/repositories/note_repository.dart';
 import 'presenter/add_or_edit_note_controller.dart';
 import 'presenter/add_or_edit_note_page.dart';
+import 'presenter/stores/expanded_store.dart';
+import 'presenter/stores/scroll_in_top_store.dart';
 
 class AddOrEditNoteModule extends Module {
   @override
@@ -32,11 +34,15 @@ class AddOrEditNoteModule extends Module {
                   i<INoteRepository>(),
                 )),
         //
+        Bind.lazySingleton<ExpandedStore>((i) => ExpandedStore()),
+        Bind.lazySingleton<ScrollInTopStore>((i) => ScrollInTopStore()),
         Bind.lazySingleton<AddOrEditNoteController>(
             (i) => AddOrEditNoteController(
                   i<IAddNoteUsecase>(),
                   i<IEditNoteUsecase>(),
                   i<IDeleteNoteEmptyUsecase>(),
+                  i<ExpandedStore>(),
+                  i<ScrollInTopStore>(),
                 )),
       ];
 
